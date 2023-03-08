1f3a2156695e5024a0a02f42ca81ce59ecca7556
Jianhao Yu <yujianhao1314@gmail.com>
2023-03-08 23:37:07 +0000

    Created three models (movie,list,bookmark) with validations and seeded from the moviedb api

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'open-uri'
+
+puts "Cleaning database..."
+Movie.destroy_all
+
+url = 'https://tmdb.lewagon.com/movie/top_rated'
+movies_serialized = URI.open(url).read
+movies = JSON.parse(movies_serialized)['results']
+
+puts "Creating movies..."
+
+movies.each do |movie|
+  Movie.create(
+    title: movie['title'],
+    overview: movie['overview'],
+    poster_url: "https://image.tmdb.org/t/p/original#{movie['poster_path']}",
+    rating: movie['vote_average']
+  )
+end
+
+puts "#{Movie.count} movies were created"
